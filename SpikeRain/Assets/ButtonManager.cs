2c6b7c5d10d856fc3d2df94188b7d7354ac17b7c
ingridsl <inus2agome@gmail.com>
2021-05-01 20:51:38 -0300

    ajustes e share

--- SpikeRain/Assets/ButtonManager.cs
+++ SpikeRain/Assets/ButtonManager.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 using System;
 using TMPro;
 using UnityEngine.UI;
+using System.IO;
 
 public class ButtonManager : MonoBehaviour
 {
@@ -82,6 +83,33 @@ public class ButtonManager : MonoBehaviour
 
     public void Share()
     {
+        StartCoroutine(TakeScreenshotAndShare());
+    }
 
+
+    private IEnumerator TakeScreenshotAndShare()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+        File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+        // To avoid memory leaks
+        Destroy(ss);
+
+        new NativeShare().AddFile(filePath)
+            .SetSubject("I played Emoji vs Spike").SetText("That was my score!").SetUrl("https://github.com/yasirkula/UnityNativeShare")
+            .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+            .Share();
+
+        //Share on WhatsApp only, if installed(Android only)
+        if (NativeShare.TargetExists("com.whatsapp"))
+        {
+            new NativeShare().AddFile(filePath).AddTarget("com.whatsapp").Share();
+        }
     }
 }
